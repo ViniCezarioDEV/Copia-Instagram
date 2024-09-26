0ec53a4f7fd1d64383bac65343bbc991afabe145
Vinicius Cezario <125317872+ViniCezarioDEV@users.noreply.github.com>
2024-09-26 17:37:10 -0300

    updating src files

--- src/componentes/conteudoLateral.js
+++ src/componentes/conteudoLateral.js
@@ -1,21 +1,30 @@
 import logo from '../images/Instagram_icon.webp';
 import './conteudoLateral.css';
+import ListItem from './listItem.js';
 
-export default function ConteudoLateral(){
-    return (
-        <div className='conteudoLateral'>
-        
-        <img src={logo} className='Logo' alt='Logo'/>
-            
-        <div>MENU
-          <ul>
-            <li>Home</li>
-            <li>Search</li>
-            <li>Explore</li>
-            <li>Messages</li>
-          </ul>
-        </div>
+import { IoHome } from "react-icons/io5";
+import { IoSearch } from "react-icons/io5";
+import { IoCompass } from "react-icons/io5";
+import { IoChatbubbleSharp } from "react-icons/io5";
 
+
+
+export default function ConteudoLateral() {
+  return (
+    <div className='conteudoLateral'>
+
+      <img src={logo} className='Logo' alt='Logo' />
+
+      <div>
+        <ul className='navagationButtons'>
+          <ListItem text='Home' icon={<IoHome />} />
+          <ListItem text='Search' icon={<IoSearch />} />
+          <ListItem text='Explore' icon={<IoCompass />} />
+          <ListItem text='Messages' icon={<IoChatbubbleSharp />} />
+
+        </ul>
       </div>
-    );
+
+    </div>
+  );
 }
